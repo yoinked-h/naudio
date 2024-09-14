ae443735eb6e68bffdbc22d7bb65adb0be737cd5
pepe10-gpu <pepe.dannyboy@gmail.com>
2024-09-14 02:25:36 -0700

    t5 part 3 (almost fully done just need to impl the true pos bucket which i might just not do since that sucks)

--- src/naudio/models/t5/model.py
+++ src/naudio/models/t5/model.py
@@ -4,76 +4,58 @@ from beartype import beartype
 from flax import nnx
 from jaxtyping import Array, Float, jaxtyped
 from naudio.models.activations import ReLU
-# most of the t5 attn code is borrowed from comfyui's implementation
-class T5Attention(nnx.Module):
-    def __init__(self, d_model, num_heads, rel_attn_bias:bool=True, rngs: nnx.Rngs|None = None) -> None:
-        if rngs is None:
-            raise ValueError("rngs must not be None")
-        self.toq = nnx.Linear(d_model, d_model, use_bias=False, rngs=rngs)
-        self.tok = nnx.Linear(d_model, d_model, use_bias=False, rngs=rngs)
-        self.tov = nnx.Linear(d_model, d_model, use_bias=False, rngs=rngs)
-        self.too = nnx.Linear(d_model, d_model, use_bias=False, rngs=rngs)
-        self.n_heads = num_heads
-        if rel_attn_bias:
-            self.relattnbias_n_buckets = 32
-            self.relattnbias_max_distance = 128
-            self.relattnbias = nnx.Embed(self.relattnbias_n_buckets, self.n_heads, rngs=rngs)
-        else:
-            self.relattnbias = None
-    @staticmethod
-    def _rel_pos_buckets(rel_pos, n_buckets=32, max_distance=128):
-        rel_buckets = 0
-        n_buckets //= 2
-        rel_buckets += (rel_pos > 0) * n_buckets
-        rel_pos = jnp.abs(rel_pos)
-        max_exact = n_buckets // 2
-        is_small = rel_pos < max_exact
-        rel_pos_if_large = max_exact + (
-            jnp.log(rel_pos / max_exact)
-            / jnp.log(max_distance / max_exact)
-            * (n_buckets - max_exact)
-        )
-        rel_pos_if_large = jnp.minimum(rel_pos_if_large, jnp.full_like(rel_pos_if_large, n_buckets - 1))
-        rel_buckets += jnp.where(is_small, rel_pos, rel_pos_if_large)
-        return rel_buckets
-    
-    def compute_bias(self, q_len, k_len):
-        assert self.relattnbias
-        ctx_pos = jnp.arange(q_len)[:, None]
-        mem_pos = jnp.arange(k_len)[None, :]
-        rel_pos = mem_pos - ctx_pos 
-        rel_pos_bucket = self._rel_pos_buckets(rel_pos, self.relattnbias_n_buckets, self.relattnbias_max_distance)
-        vals = self.relattnbias(rel_pos_bucket)
-        return vals
-    def __call__(self, x, mask=None):
-        q = self.toq(x)
-        k = self.tok(x)
-        v = self.tov(x)
-        bias = None
-        if self.relattnbias is not None:
-            bias = self.compute_bias(x.shape[1], x.shape[1])
-        if bias is not None:
-            if mask is not None:
-                mask = mask + bias
-            else:
-                mask = bias
-        out = nnx.dot_product_attention(q, k * ((k.shape[-1] / self.n_heads) ** 0.5), v, mask=mask)
-        out = (
-            out.transpose(1,2).reshape(q.shape[0], -1, self.n_heads * q.shape[-1])
-        )
-        return self.too(out), bias
+
 
 class T5SelfAttention(nnx.Module):
     def __init__(self, d_model, num_heads, rel_attn_bias:bool=True, rngs: nnx.Rngs|None = None) -> None:
         if rngs is None:
             raise ValueError("rngs must not be None")
-        self.self_attn = T5Attention(d_model, num_heads, rel_attn_bias, rngs=rngs)
+        self.n_heads = num_heads
+        self.d_model = d_model
+        self.self_attn = nnx.MultiHeadAttention(num_heads=num_heads, in_features=d_model, out_features=d_model, qkv_features=d_model, use_bias=rel_attn_bias, decode=False, rngs=rngs)
         self.ln = nnx.LayerNorm(d_model, rngs=rngs)
-    
+        val = jnp.ones((num_heads, 12)) #todo, make the init random
+        self.relative_attn_bias = nnx.Param(value=val)
+            
+    @staticmethod
+    def _rel_pos_bucket(relative_position, bidirectional=True, num_buckets=12, max_distance=128):
+        ret = 0
+        n = -relative_position
+        if bidirectional:
+            num_buckets //= 2
+            ret += (n < 0).astype(jnp.int32) * num_buckets
+            n = jnp.abs(n)
+        else:
+            n = jnp.maximum(n, 0)
+        max_exact = num_buckets // 2
+        is_small = n < max_exact
+        val_if_large = max_exact + (
+            jnp.log(
+                n.astype(jnp.float32) / max_exact +
+                jnp.finfo(jnp.float32).eps) /
+            jnp.log(max_distance / max_exact) *
+            (num_buckets - max_exact)).astype(jnp.int32)
+        val_if_large = jnp.minimum(val_if_large, num_buckets - 1)
+        ret += jnp.where(is_small, n, val_if_large)
+        return ret
+    def calc_relative_position(self, query_length, key_length):
+        
+        # archaic code from linen
+        
+        context_position = jnp.arange(query_length, dtype=jnp.int32)[:, None]
+        memory_position = jnp.arange(key_length, dtype=jnp.int32)[None, :]
+        relative_position = memory_position - context_position  
+        rp_bucket = self._rel_pos_bucket(relative_position)
+        ohot = jax.nn.one_hot(rp_bucket, self.relative_attn_bias.shape[-1])
+        out = jnp.matmul(ohot, self.relative_attn_bias.value)  
+
+        return out
     def __call__(self, x, mask=None):
         normed = self.ln(x)
-        attn_output, bias = self.self_attn(normed, mask=mask)
-        return x + attn_output, bias
+        # bias = self.calc_relative_position(x.shape[1], x.shape[1])
+        attn_output = self.self_attn(normed, mask=mask)
+        # attn_output = attn_output + bias
+        return x + attn_output
 class T5EncoderLayer(nnx.Module):
     def __init__(self, d_model, d_ff, num_heads, dropout_rate, rngs: nnx.Rngs|None = None) -> None:
         if rngs is None:
@@ -86,7 +68,7 @@ class T5EncoderLayer(nnx.Module):
         )
         self.layer_norm1 = nnx.LayerNorm(d_model, rngs=rngs)
         self.layer_norm2 = nnx.LayerNorm(d_model, rngs=rngs)
-        self.dropout = nnx.Dropout(dropout_rate)
+        self.dropout = nnx.Dropout(dropout_rate, rngs=rngs)
 
     def __call__(self, x, mask=None):
         attn_output = self.self_attn(x, mask)
@@ -112,8 +94,12 @@ class T5Encoder(nnx.Module):
 if __name__ == "__main__":
     print("testing t5")
     rngs = nnx.Rngs(0x5f16)
-    enc = T5Encoder(12, 768, 3072, 12, 0.1, rngs=rngs)
-    x = jnp.ones((2**16, 768))
+    MODEL_DIM = 768
+    LAYERS = 12
+    HEADS = 12
+    DROPOUT = 0.1
+    enc = T5Encoder(num_layers=LAYERS, d_model=MODEL_DIM, d_ff=MODEL_DIM*3, num_heads=HEADS, dropout_rate=DROPOUT, rngs=rngs)
+    x = jnp.ones((1, 3, MODEL_DIM))
     print(x.shape)
     enc = enc(x)
     print(enc.shape)
